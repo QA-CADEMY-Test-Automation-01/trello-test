a6fccc8b51862ed251bd20d6d3363bf135e7eb3f
Luis Cachi Condori <Luis.Cachi@jalasoft.com>
2020-12-17 17:47:23 -0400

    first commit

---
+++ lib/src/test/java/trello/test/GoRestTest.java
@@ -0,0 +1,51 @@
+package trello.test;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GoRestTest {
+
+    int newuserID;
+
+    private RequestSpecification requestSpecification;
+
+    public  GoRestTest() {
+        requestSpecification = new RequestSpecBuilder().
+                setBaseUri("https://gorest.co.in").
+                addHeader("Authorization", "Bearer ee74c94191ca177aefa1a9eb7e2b538cf6e886cd4763095bf7d4e89b5bc631fb").
+                addHeader("Content-Type", "application/json").
+                build();
+    }
+
+//    @Test
+//    public void GetUsers(){
+//        given().spec(requestSpecification).
+//                when().log().all().
+//                get("/public-api/users").
+//                then().log().all().
+//                assertThat().statusCode(200);
+//    }
+
+    @Test
+    public void PostUser(){
+        newuserID = given().spec(requestSpecification).
+                body("{\"name\":\"Tenali Ramakrishna\", \"gender\":\"Male\", \"email\":\"tenali.dd@15ce.com\", \"status\":\"Active\"}").
+                when().
+                post("/public-api/users").
+                then().log().all().
+                assertThat().statusCode(200).extract().response().path("data.id");
+    }
+
+    @Test
+    public void GetUser(){
+        String response = given().spec(requestSpecification).log().all().
+                when().
+                get("/public-api/users/{id}", newuserID).
+                then().extract().response().path("data.email");
+        Assert.assertEquals("tenali.dd@15ce.com" , response);
+    }
+}
---
+++ lib/src/test/java/trello/test/Trello.java
@@ -0,0 +1,83 @@
+package trello.test;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Trello {
+    private RequestSpecification requestSpecification;
+    private ResponseSpecification responseSpecification;
+
+    public Trello() {
+        requestSpecification = new RequestSpecBuilder().
+                setBaseUri("https://api.trello.com").
+                addHeader("Content-Type", "application/json").
+                addQueryParam("key", "013fb1d80492537963ac96f2ec4eaf83").
+                addQueryParam("token", "3da9a29af840e034223fef312e0123d43b70c6bd9d149adc4df4e4374e79bafa").
+                build();
+
+        responseSpecification = new ResponseSpecBuilder().
+                expectStatusCode(200).
+                expectContentType(ContentType.JSON).
+                build();
+    }
+
+    @Test
+    public void testTrelloBoardsGetMethod() {
+        given().
+                spec(requestSpecification).
+                log().all().
+                when().
+                get("/1/members/me/boards").
+                then().
+                log().all().
+                spec(responseSpecification);
+    }
+
+    @Test
+    public void testTrelloBoardsPostMethod() {
+        given().
+                spec(requestSpecification).
+                body("{\"name\":\"Example Board\"}").
+                log().all().
+                when().
+                post("/1/boards").
+                then().
+                log().all().
+                spec(responseSpecification).
+                assertThat().
+                body("name", equalTo("Example Board"));
+
+    }
+
+    @Test
+    public void testTrelloBoardsPutMethod() {
+        given().
+                spec(requestSpecification).
+                body("{\"name\":\"Renamed Example\"}").
+                log().all().
+                when().
+                put("/1/boards/{id}","5f5426601f240e861780f51d").
+                then().
+                log().all().
+                spec(responseSpecification);
+    }
+
+    @Test
+    public void testTrelloBoardsDeleteMethod() {
+        given().
+                spec(requestSpecification).
+                log().all().
+                when().
+                delete("/1/boards/{id}","5f5426601f240e861780f51d").
+                then().
+                log().all().
+                spec(responseSpecification);
+    }
+}
